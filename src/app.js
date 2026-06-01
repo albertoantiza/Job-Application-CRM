238a443c51412183f5842735225976edbb5d3643
/elchopo7 <elchopo7@gmail.com>
2026-06-01 18:23:36 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,2 +1,17 @@
+import express from 'express'
+import jobsRoutes from './routes/jobs.routes.js'
+import { errorHandler } from './middlewares/errorHandler.js'
 
-// this is a comment
+const app = express()
+
+app.use(express.json())
+
+app.get('/health', (req, res) => {
+  res.status(200).json({ ok: true })
+})
+
+app.use('/jobs', jobsRoutes)
+
+app.use(errorHandler)
+
+export default app
