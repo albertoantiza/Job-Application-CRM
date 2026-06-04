b497469fc83ae6582e313e2f9221fe90e4d30059
/elchopo7 <elchopo7@gmail.com>
2026-06-04 12:37:25 +0200

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,16 @@
 import { Router } from 'express'
 import applicationsRoutes from './applications.routes.js'
+import contactsRoutes from './contacts.routes.js'
+import companiesRoutes from './companies.routes.js'
+import interviewsRoutes from './interviews.routes.js'
+import notesRoutes from './notes.routes.js'
 
 const router = Router()
 
 router.use('/applications', applicationsRoutes)
+router.use('/contacts', contactsRoutes)
+router.use('/companies', companiesRoutes)
+router.use('/interviews', interviewsRoutes)
+router.use('/notes', notesRoutes)
 
 export default router
