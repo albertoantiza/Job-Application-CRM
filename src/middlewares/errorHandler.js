c20c7abf8754fa625959b7ac50e1f9d20bfbeda4
/elchopo7 <elchopo7@gmail.com>
2026-06-04 11:52:31 +0200

    Update errorHandler.js

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,16 @@
+import ApiError from '../utils/ApiError.js'
+import { logger } from '../utils/logger.js'
+
+export const errorHandler = (err, req, res, next) => {
+  if (err instanceof ApiError) {
+    return res.status(err.statusCode).json({
+      error: err.message
+    })
+  }
+
+  logger.error(err)
+
+  return res.status(500).json({
+    error: 'Internal Server Error'
+  })
+}
